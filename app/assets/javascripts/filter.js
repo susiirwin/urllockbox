7b3302491da3231b393ec3cab9a221393f3cdcaf
SusiIrwin <susi.irwin@gmail.com>
2017-01-08 15:19:22 -0700

    add search field

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -24,9 +24,20 @@ $(document).ready(function(){
   })
 
   $('#show-all').on('click', function(){
-
     $('.link').each(function(index, link){
         $(link).show();
       })
     })
+
+  $('#search-box').on('keyup', function(){
+    var currentSearch = $(this).val().toUpperCase();
+
+    $('.link').each(function(index, link) {
+      if (link.children[1].innerText.toUpperCase().indexOf(currentSearch) !== -1) {
+        $(link).show();
+      } else {
+        $(link).hide();
+      }
+    })
+  })
 })
